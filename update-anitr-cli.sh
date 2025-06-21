311f425aa236fd6fbf8d2cfc0a5873f5b9881524
xeyossr <113219171+xeyossr@users.noreply.github.com>
2025-06-21 08:31:47 +0400

    update

--- update-anitr-cli.sh
+++ update-anitr-cli.sh
@@ -4,7 +4,7 @@ set -e
 
 REPO="xeyossr/anitr-cli"
 BINARY_NAME="anitr-cli"
-TMP_PATH="/tmp/$BINARY_NAME"
+TMP_PATH="/tmp/$BINARY_NAME-new"
 INSTALL_PATH="/usr/bin/$BINARY_NAME"
 
 echo "🔄 Yeni sürüm indiriliyor..."
@@ -15,11 +15,8 @@ wget -q -O "$TMP_PATH" "$LATEST_URL"
 chmod +x "$TMP_PATH"
 
 echo "📁 Kurulum dizinine yazılıyor..."
+sleep 1
 
-if [ "$EUID" -ne 0 ]; then
-    sudo cp "$TMP_PATH" "$INSTALL_PATH"
-else
-    cp "$TMP_PATH" "$INSTALL_PATH"
-fi
+sudo mv "$TMP_PATH" /usr/bin/anitr-cli
 
 echo "✅ anitr-cli başarıyla güncellendi!"
