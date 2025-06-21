a8aa706b6c0fe5d90e6868957a931c1320cc4dd5
xeyossr <113219171+xeyossr@users.noreply.github.com>
2025-06-21 08:10:55 +0400

    UA ve Referrer headerleri eklendi

--- modules/player.py
+++ modules/player.py
@@ -3,7 +3,13 @@ import subprocess, time
 def open_with_video_player(url, subtitle_url=None):
     """Video Oynatıcı"""
     try:
-        cmd = ['mpv', '--fullscreen']
+        cmd = [
+            'mpv',
+            '--fullscreen',
+            '--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 Chrome/137.0.0.0 Safari/537.36',
+            '--referrer=https://yeshi.eu.org/'
+        ]
+
         if subtitle_url:
             cmd += ['--sub-file=' + subtitle_url]
         cmd.append(url)
