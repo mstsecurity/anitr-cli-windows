d60d367dab879c18a988911301e9e8f346e0dfec
Mstsec <mailspammer444@gmail.com>
2025-07-07 21:53:15 +0300

    config.py

--- modules/config.py
+++ modules/config.py
@@ -2,28 +2,41 @@ import os
 import time
 from dotenv import load_dotenv
 import modules.utils as utils
-load_dotenv(os.path.expanduser("~/.config/anitr-cli/config"))
 
-default_ui = utils.get_env("default_ui", "DEFAULT_UI", default="tui")
+_user_home = os.path.expanduser("~")
+_anitr_config_dir = os.path.join(_user_home, ".anitr-cli")
+_config_file_path = os.path.join(_anitr_config_dir, "config")
+
+if not os.path.exists(_anitr_config_dir):
+    os.makedirs(_anitr_config_dir, exist_ok=True)
+
+load_dotenv(_config_file_path)
+
 discord_rpc = utils.get_env("discord_rpc", "DISCORD_RPC", default="enabled")
 save_position_on_quit = utils.get_bool_env(
     "save_position_on_quit", "SAVE_POSITION_ON_QUIT", default="false")
 
-sources = ["AnimeciX (anm.cx)", "OpenAnime (openani.me)"]  # Kaynaklar
+default_ui = "tui" 
 
-# Global Discord RPC durumu ve zaman
+sources = ["AnimeciX (anm.cx)", "OpenAnime (openani.me)"]
+
 discord_client_id = "1383421771159572600"
 rpc = None
 rpc_initialized = False
 start_time = int(time.time())
 
-# Paths
-anime_details = "/tmp/anime_details"
-error_log = "/tmp/anitr-cli-error.log"
-config_path = os.path.expanduser("~/.config/anitr-cli/config")
+_temp_dir = os.environ.get('TEMP') or os.environ.get('TMP')
+if not _temp_dir:
+    _temp_dir = os.path.join(_user_home, "anitr_temp")
+    if not os.path.exists(_temp_dir):
+        os.makedirs(_temp_dir, exist_ok=True)
 
-# GitHub
+anime_details = os.path.join(_temp_dir, "anime_details.json")
+error_log = os.path.join(_temp_dir, "anitr-cli-error.log")
+config_path = _config_file_path
+
 github_repo = "https://github.com/xeyossr/anitr-cli"
 
 GITHUB_REPO = "xeyossr/anitr-cli"
+
 CURRENT_VERSION = "3.6.0"
