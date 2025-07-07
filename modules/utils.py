fdfc95e53c8a30466cc7415b7bb10807e9e709e8
Mstsec <mailspammer444@gmail.com>
2025-07-07 22:44:05 +0300

    utils.py

--- modules/utils.py
+++ modules/utils.py
@@ -9,7 +9,6 @@ try:
 except ImportError:
     _WINDOWS_NOTIFICATIONS_AVAILABLE = False
 
-
 import modules.config as config
 
 
@@ -42,6 +41,5 @@ def smart_print(text: str, notification_msg: str, notification: bool = True, ico
     print(text)
 
 def get_source(ui_module):
-    
     return ui_module.select_menu("tui", config.sources, "Kaynak seç:", False)
 
